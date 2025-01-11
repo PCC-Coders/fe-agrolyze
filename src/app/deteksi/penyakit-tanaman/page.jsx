1e9541e62fb61538dd4cae22d6c9b02d5e9b38d3
Wrseno <warsenosetyono@gmail.com>
2025-01-11 09:21:03 +0700

    feat: add deteksi

---
+++ src/app/deteksi/penyakit-tanaman/page.jsx
@@ -0,0 +1,32 @@
+import {FileInputFlowbite} from "@/components/atoms/flowbite/FileInput";
+
+const PenyakitTanaman = () => {
+  return (
+    <section className='bg-agro-green text-white'>
+      <div className='p-20'>
+        <div className='grid grid-cols-2 gap-8 items-center'>
+          <FileInputFlowbite />
+          <div>
+            <h2 className='text-2xl lg:text-3xl text-center font-bold my-16'>
+              Cek <span className='text-agro-yellow'>Kesehatan</span> Tanaman
+            </h2>
+            <div className='grid gap-2'>
+              <input
+                type='text'
+                name=''
+                id=''
+                className='rounded-lg bg-agro-green'
+                placeholder='Your location'
+              />
+              <button className='bg-agro-light-yellow text-agro-green rounded-lg px-4 py-2 mt-4'>
+                Cek Tanaman
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PenyakitTanaman;
