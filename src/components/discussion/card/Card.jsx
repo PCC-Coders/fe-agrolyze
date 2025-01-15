5204d99f7ce776d1f018b46a2e90843e469f12e5
Wrseno <warsenosetyono@gmail.com>
2025-01-15 11:41:22 +0700

    update halaman profile dan bookmark

--- src/components/discussion/card/Card.jsx
+++ src/components/discussion/card/Card.jsx
@@ -1,14 +1,36 @@
+"use client";
+
+import {useEffect, useState} from "react";
 import CardContent from "./CardContent";
 import data from "./CardData";
 
 export default function CardDiscussion() {
-    return (
-        <div className="space-y-4">
-            {data.map((item, index) => (
-                <div key={index} className="bg-agro-light-gray p-4 rounded-lg shadow-md max-w-3xl">
-                    <CardContent {...item} />
-                </div>
-            ))}
+  const [discusses, setDiscusses] = useState([]);
+  const [errorDiscussion, setErrorDiscussion] = useState(null);
+
+  useEffect(() => {
+    const fetchDiscussion = async () => {
+      try {
+        const response = await axios.get(`http://127.0.0.1:8000/api/discusses`);
+        const {data} = await response.json();
+        setDiscusses(data);
+      } catch (error) {
+        setErrorDiscussion("Gagal dalam mengambil data diskusi");
+      }
+    };
+
+    fetchDiscussion();
+  });
+  return (
+    <div className='space-y-4'>
+      {data.map((item, index) => (
+        <div
+          key={index}
+          className='bg-agro-light-gray p-4 rounded-lg shadow-md max-w-3xl'
+        >
+          <CardContent {...item} />
         </div>
-    );
+      ))}
+    </div>
+  );
 }
